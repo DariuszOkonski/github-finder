a4ef903540ceb0ac06ed90c8a5c531a1be849032
Dariusz Okonski <darek200180@gmail.com>
2020-02-17 14:50:53 +0100

    add: propTypes and defaultProps

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const UserItem = (props) => {
-  const { login, avatar_url, html_url } = props.user;
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 
   return (
-    <div className="card text-center">
+    < div className="card text-center" >
       <img
         src={avatar_url}
         alt={login}
@@ -16,8 +16,18 @@ const UserItem = (props) => {
       <div>
         <a href={html_url} className="btn btn-dark btn-sm">More</a>
       </div>
-    </div>
+    </div >
   );
+}
+
+UserItem.defaultProps = {
+  login: 'unknown',
+  avatar_url: 'https://cdn.pixabay.com/photo/2014/10/07/13/48/mt-fuji-477832__340.jpg',
+  html_url: 'https://github.com'
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
 }
 
 export default UserItem;
