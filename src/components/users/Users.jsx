834579ac2e758351949ac35b04462d4b786cea08
Dariusz Okonski <darek200180@gmail.com>
2020-02-17 14:51:31 +0100

    add: order in users objects properties

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -5,20 +5,20 @@ class Users extends Component {
   state = {
     users: [
       {
-        "login": "mojombo",
         "id": 1,
+        "login": "mojombo",
         "avatar_url": "https://avatars0.githubusercontent.com/u/1?v=4",
         "html_url": "https://github.com/mojombo",
       },
       {
-        "login": "defunkt",
         "id": 2,
+        "login": "defunkt",
         "avatar_url": "https://avatars0.githubusercontent.com/u/2?v=4",
         "html_url": "https://github.com/defunkt",
       },
       {
-        "login": "pjhyett",
         "id": 3,
+        "login": "pjhyett",
         "avatar_url": "https://avatars0.githubusercontent.com/u/3?v=4",
         "html_url": "https://github.com/pjhyett",
       },
