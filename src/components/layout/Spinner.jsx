32ce3ab1df07b9ba2b3153a6e91dc83de8d799bb
Dariusz Okonski <darek200180@gmail.com>
2020-02-19 20:00:16 +0100

    add: Spinner component

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+
+const Spinner = () => {
+  return (
+    <Fragment>
+      <img style={
+        {
+          width: '200px',
+          margin: 'auto',
+          display: 'block'
+        }} src={spinner} alt="Loading..." />
+    </Fragment>
+  );
+}
+
+export default Spinner;
