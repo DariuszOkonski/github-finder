ad2747946f367c098722daf06022e4676184bc99
Dariusz Okonski <darek200180@gmail.com>
2020-02-21 11:39:00 +0100

    add: getUserRepos as a props

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -2,22 +2,26 @@ import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Repos from '../repos/Repos';
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool.isRequired,
+    repos: PropTypes.array.isRequired,
     user: PropTypes.object.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
   }
 
   render() {
     const { name, avatar_url, location, bio, blog, login, html_url, company, followers, following, public_repos, public_gists, hireable } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading)
       return <Spinner />
@@ -96,6 +100,7 @@ class User extends Component {
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
 
+        <Repos repos={repos} />
       </Fragment>
     );
   }
