26f3ef14b79e0c222f1f3718b96271f4450f3e7f
Dariusz Okonski <darek200180@gmail.com>
2020-02-21 11:39:27 +0100

    add: getUserRepos method

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   }
@@ -45,6 +46,19 @@ class App extends Component {
     });
   }
 
+  getUserRepos = async (username) => {
+    this.setState({
+      loading: true
+    })
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({
+      repos: res.data,
+      loading: false,
+    });
+  }
+
   clearUsers = () => {
     this.setState({
       users: [],
@@ -68,7 +82,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading, user } = this.state;
+    const { users, loading, user, repos } = this.state;
 
     return (
       <Router basename={process.env.PUBLIC_URL}>
@@ -103,6 +117,8 @@ class App extends Component {
                 <User
                   {...props}
                   getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  repos={repos}
                   user={user}
                   loading={loading}
                 />}
