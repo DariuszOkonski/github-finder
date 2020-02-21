03105bf7983db566dbecdd6f95c853105c56dc3f
Dariusz Okonski <darek200180@gmail.com>
2020-02-21 11:37:51 +0100

    add: RepoItem Repos

---
+++ src/components/repos/Repos.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import RepoItem from './RepoItem';
+import PropTypes from 'prop-types';
+
+const Repos = ({ repos }) => {
+  return repos.map(repo =>
+    <RepoItem
+      repo={repo}
+      key={repo.node_id}
+    />
+  )
+}
+
+Repos.propType = {
+  repos: PropTypes.array.isRequired
+}
+
+export default Repos;
